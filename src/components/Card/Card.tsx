9a861e9d40e70a353fc866f13563bf92288f1625
Muniox <muniox20@gmail.com>
2023-01-28 13:11:54 +0100

    fix accesibility add section and footer name

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,7 +5,7 @@ import hero from '../../assets/illustration-hero.svg';
 
 export const Card: FC = () => {
     return (
-    <div className="card">
+    <section className="card">
         <img className="card__image" src={hero} alt="" />
         <h1 className="card__title">Order Summary</h1>
   
@@ -15,6 +15,6 @@ export const Card: FC = () => {
         <button className="card__button" type="submit">Proceed to Payment</button>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <div><a href="#" className="card__cancel-link">Cancel Order</a></div>
-    </div>
+    </section>
     );
 };
